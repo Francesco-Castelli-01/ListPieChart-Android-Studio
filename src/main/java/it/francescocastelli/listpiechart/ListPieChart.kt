0cd0ae645e0e35328318beb409f2c09be1c4ef07
Francesco-Castelli-01 <bulloBULLO123>
2020-06-02 11:29:07 +0200

    corrections 3

--- src/main/java/it/francescocastelli/listpiechart/ListPieChart.kt
+++ src/main/java/it/francescocastelli/listpiechart/ListPieChart.kt
@@ -1,12 +1,11 @@
 package it.francescocastelli.listpiechart
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.updateBounds
 
 class ListPieChart @JvmOverloads constructor(
     viewContext: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -20,13 +19,19 @@ class ListPieChart @JvmOverloads constructor(
     private fun executeProportion(value: Float,sumOfValues:Float):Float{
         return value*360/sumOfValues
     }
+    //private val clockHandPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    /**private val clockHand = ContextCompat.getDrawable(context,R.drawable.clock_hand)
+    private val rect:Rect = Rect()
+    private val handDimension = 25**/
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        println("ON DRAW CALLEd")
         val centerWidth = (width/2).toFloat()
         val centerHeight = (height/2).toFloat()
-        val totalRadiusCircle = 85f
+
         val arcDepth = 5f
+        val totalRadiusCircle = (if (width<height) width else height)/2-arcDepth
 
         rectF.apply {
             left = centerWidth-totalRadiusCircle - arcDepth
@@ -40,12 +45,14 @@ class ListPieChart @JvmOverloads constructor(
             if (data.isNotEmpty()){
                 for (i in data.indices){
                     if (i==0)
-                        drawArc(rectF,0f,executeProportion(data[i].value,sumOfValues),true,listOfPaints[i]) // x:360 = entry:sumOfValues -> x=360*entry/sumOfAll
+                        drawArc(rectF,270f,executeProportion(data[i].value,sumOfValues),true,listOfPaints[i]) // x:360 = entry:sumOfValues -> x=360*entry/sumOfAll
                     else{
                         val sumOfPreviousValues = data.subList(0,i).map { it.value }.sum()
                         //this command gets the values of the previous values to determine the startAngle
 
-                        val startAngle = executeProportion(sumOfPreviousValues,sumOfValues)
+                        var startAngle = executeProportion(sumOfPreviousValues,sumOfValues)+270
+                        if (startAngle>360)
+                            startAngle-=360
 
                         val sweepAngle = executeProportion(data[i].value,sumOfValues)
 
@@ -53,11 +60,18 @@ class ListPieChart @JvmOverloads constructor(
                     }
                 }
             }
-
             drawCircle(centerWidth,centerHeight,totalRadiusCircle,whiteCornerPaint)
             primaryColorPaint.color = pieColor
-            drawCircle(centerWidth,centerHeight,80f,primaryColorPaint)
-
+            drawCircle(centerWidth,centerHeight,totalRadiusCircle-arcDepth,primaryColorPaint)
+            /**clockHand!!.bounds = rect
+            clockHand.updateBounds(
+                left = width/2-handDimension,
+                right = width/2+handDimension,
+                top = height/10 + (height/2)-totalRadiusCircle.toInt(),
+                bottom = centerHeight.toInt()
+            )
+            clockHand.draw(canvas)
+            **/
         }
     }
 
